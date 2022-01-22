04d33b09da8c0ec61ba59bb422974c1965587984
Özgür Öztürk <56519674+BestInSpire@users.noreply.github.com>
2022-01-22 12:47:33 +0300

    Delete script.js

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-let menuOpenBtn = document.querySelector(".navbar .bx.bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-let navLinks = document.querySelector(".nav-links")
-console.log('a')
-menuOpenBtn.addEventListener("click", () => {
-    alert()
-    document.querySelector(".nav-links").style.left = "0"
-})
